9195413da651411f148dea684d05bfb525ccbddc
Kamil Falkowski <falkowskikamil97@wp.pl>
2023-10-07 11:12:42 +0200

    Finish permissions

--- blango/settings.py
+++ blango/settings.py
@@ -207,13 +207,18 @@ class Dev(Configuration):
         "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
     ]
 
-    # Rest Token
+    # Rest Token and permission
     REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.BasicAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
-    ]}
+        "DEFAULT_AUTHENTICATION_CLASSES": [
+            "rest_framework.authentication.BasicAuthentication",
+            "rest_framework.authentication.SessionAuthentication",
+            "rest_framework.authentication.TokenAuthentication",
+        ],
+        "DEFAULT_PERMISSION_CLASSES": [
+            "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+        ],
+    }
+
 
 
 
